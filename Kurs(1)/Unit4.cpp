6768ec4a5224e256f82288b5f8e1cda6eae49b32
UberPPP <luberpppl@gmail.com>
2017-05-07 13:26:18 +0700

    Start
    1 day

---
+++ Kurs(1)/Unit4.cpp
@@ -0,0 +1,24 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "Unit4.h"
+#include "Unit2.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm4 *Form4;
+//---------------------------------------------------------------------------
+__fastcall TForm4::TForm4(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm4::BitBtn1Click(TObject *Sender)
+{
+	int num_side=4;
+	Form2->Edit1->Text = num_side;
+	Close();
+}
+//---------------------------------------------------------------------------
